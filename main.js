7466cb84763185a4fa352ba551eefedfd5fb2d2e
Thomas-Basadonne <thommybasa@gmail.com>
2023-02-21 13:00:57 +0100

    Fine grafica + 2 slider

---
+++ main.js
@@ -0,0 +1,62 @@
+const { createApp } = Vue;
+createApp({
+  data() {
+    return {
+      links: ["Home", "About Us", "Trainigns", "Packages", "Blog", "Contact"],
+      trainings: [
+        "Riding Lesson",
+        "Safe Driving",
+        "Mountain Bike",
+        "Trail Drive",
+        "Pedaling",
+      ],
+      jumbo: [
+        {
+          image: "img/slider-bike-4.jpg",
+          title: "Professional Cycling Club",
+          text: "Learn cycling from the pros.",
+        },
+        {
+          image: "img/slider-bike-9.jpg",
+          title: "Ride with the Champions",
+          text: "National Mountain-Bike team.",
+        },
+        {
+          image: "img/slider-bike-12.jpg",
+          title: "The best Bike Blog",
+          text: "Learn cycling from the Blog.",
+        },
+      ],
+      courses: [
+        {
+          image: "img/slider-bike-9.jpg",
+          title: "Ride with the Champions",
+          text: "National Mountain-Bike team.",
+        },
+      ],
+      activeImage: 0,
+      hover: false,
+    };
+  },
+  methods: {
+    prevImage() {
+      if (this.activeImage - 1 < 0) {
+        this.activeImage = 2;
+      } else {
+        this.activeImage--;
+      }
+    },
+    nextImage() {
+      if (this.activeImage + 1 > 2) {
+        this.activeImage = 0;
+      } else {
+        this.activeImage++;
+      }
+    },
+    switchToImage(index) {
+      this.activeImage = index;
+    },
+  },
+
+  mounted() {},
+}).mount("#app");
